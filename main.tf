7bcc92caab35b60d203500fba092d1daae063f23
Sreyasee97 <sreyaseepaharijgm@gmail.com>
2024-07-30 22:29:52 +0530

    second commit

--- main.tf
+++ main.tf
@@ -16,10 +16,9 @@ provider "azurerm" {
 resource "azurerm_resource_group" "example" {
   name     = "example"
   location = "West US 2"
+  
 }
 resource "azurerm_resource_group" "example1" {
   name     = "example1"
   location = "West US 2"
-  
-  
 }
